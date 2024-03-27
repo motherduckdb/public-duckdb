ecd2718813e6c82b52d83183f3eaa72b6f410932
Florian Gerlinghoff <florian@motherduck.com>
2024-03-27 22:21:34 +0100

    Add (de)serialization for CopyDatabaseInfo

--- src/include/duckdb/parser/parsed_data/copy_database_info.hpp
+++ src/include/duckdb/parser/parsed_data/copy_database_info.hpp
@@ -19,12 +19,22 @@ public:
 	static constexpr const ParseInfoType TYPE = ParseInfoType::COPY_DATABASE_INFO;
 
 public:
-	CopyDatabaseInfo(const std::string &to_database)
+	explicit CopyDatabaseInfo() : ParseInfo(TYPE), to_database(INVALID_CATALOG) {
+	}
+
+	explicit CopyDatabaseInfo(const std::string &to_database)
 	: ParseInfo(TYPE), to_database(to_database) {
 	}
 
+	// The destination database to which catalog entries are being copied
 	std::string to_database;
+
+	// The catalog entries that are going to be created in the destination DB
 	vector<unique_ptr<CreateInfo>> entries;
+
+public:
+	void Serialize(Serializer &serializer) const override;
+	static unique_ptr<ParseInfo> Deserialize(Deserializer &deserializer);
 };
 
 } // namespace duckdb
